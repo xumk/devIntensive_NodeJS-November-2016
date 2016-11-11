f134981e7e3f7e7e3cee662501be15e73637822d
xumk <xumuk123@mail.ru>
2016-11-11 12:32:42 +0300

    Рефакторинг задачи 2B

--- src/index.js
+++ src/index.js
@@ -4,43 +4,55 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+    res.json({
+        hello: 'JS World',
+    });
 });
 
-app.get('/lesson2/2A', (req, res) =>{
-  const a = req.query.a? parseInt(req.query.a) : 0;
-  const b = req.query.b? parseInt(req.query.b) : 0;
-  const sum = a + b;
-  res.send(sum.toString());
+app.get('/lesson2/2A', (req, res) => {
+    const a = req.query.a ? parseInt(req.query.a) : 0;
+    const b = req.query.b ? parseInt(req.query.b) : 0;
+    const sum = a + b;
+    res.send(sum.toString());
 });
 
-String.prototype.capitalizeFirstLetter = function() {
+String.prototype.capitalizeFirstLetter = function () {
     return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 };
 
-app.get('/lesson2/2B', (req, res) =>{
-  const reg = new RegExp('[A-zА-яЁё]');
-  let result = "Invalid fullname";
-  const fullname = req.query.fullname.trim();
-  if (fullname && !/[0-9]/.test(fullname) && !/_/.test(fullname) && !/\//.test(fullname)) {
-      const names = fullname.split(/\s+/);
-      const length = names.length;
-      switch(length) {
-          case 1:
-              result = names[0].capitalizeFirstLetter();
-              break;
-          case 2:
-              result = names[1].capitalizeFirstLetter() + " " + names[0].capitalizeFirstLetter().charAt(0) + ".";
-              break;
-          case 3:
-              result = names[2].capitalizeFirstLetter() + " " + names[0].capitalizeFirstLetter().charAt(0) + ". " + names[1].capitalizeFirstLetter().charAt(0) + ".";
-      }
-  }
-  res.send(result);
+app.get('/lesson2/2B', (req, res) => {
+    const reg = new RegExp('[A-zА-яЁё]');
+    let result = "Invalid fullname";
+    const fullname = req.query.fullname.trim();
+    if (fullname && !/[0-9]/.test(fullname) && !/_/.test(fullname) && !/\//.test(fullname)) {
+        const names = fullname.split(/\s+/);
+        const length = names.length;
+        let lastName;
+        let firstName;
+        let middleName;
+        switch (length) {
+            case 1:
+                result = names[0].capitalizeFirstLetter();
+                break;
+            case 2:
+                lastName = names[1].capitalizeFirstLetter();
+                firstName = names[0].capitalizeFirstLetter();
+                result = lastName + " " + getFirstChar(firstName) + ".";
+                break;
+            case 3:
+                lastName = names[2].capitalizeFirstLetter();
+                firstName = names[0].capitalizeFirstLetter();
+                middleName = names[1].capitalizeFirstLetter();
+                result = lastName + " " + getFirstChar(firstName) + ". " + getFirstChar(middleName) + ".";
+        }
+    }
+    res.send(result);
 });
 
+function getFirstChar(str) {
+    return str.charAt(0);
+}
+
 app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
+    console.log('Your app listening on port 3000!');
 });
